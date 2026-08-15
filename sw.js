c5912a84160d4f21beb54f7f56150e65416d1571
forte3dvfx-code <forte3dvfx@gmail.com>
2026-08-15 16:09:18 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    a versão antiga.
    ================================================================== */
 
-const CACHE_NAME = "caderno-leitura-v7";
+const CACHE_NAME = "caderno-leitura-v8";
 
 const APP_FILES = [
   "./",
